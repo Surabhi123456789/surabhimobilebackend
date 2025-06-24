d86af511bcc24ca7c05234342dcbe1d25a184a04
Surabhi123456789 <110608393+Surabhi123456789@users.noreply.github.com>
2025-06-24 11:13:42 +0530

    updated cors

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const dotenv = require("dotenv");
 const cloudinary = require("cloudinary");
 const connectDatabase = require("./config/database")
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://surabhimobilefrontend.vercel.app", // allow your frontend domain
+  methods: ["GET", "POST", "PUT", "DELETE"],           // allow desired HTTP methods
+  credentials: true                                    // allow cookies if used
+};
+
+app.use(cors(corsOptions));
 
 process.on("uncaughtException",(err)=>{
     console.log(`Error: ${err.message}`);
